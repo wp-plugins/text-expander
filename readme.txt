bc2d72505320c56803a2ba3b21e12a4aea5db8a1
Hedge <Hedge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-14 09:17:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/text-expander/trunk@332363 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,15 +1,15 @@
-=== Text Expander ===
+Ôªø=== Text Expander ===
 Contributors: Hedge
 Tags: posts, content
 Requires at least: 2.0.0
-Tested up to: 3.0.1
-Stable tag: 0.1
+Tested up to: 3.0.4
+Stable tag: 0.2
 
-Allows you to define areas of text that expand/collapse when clicked. It improves Dagon Design's Expanding Text-Plugin.
+Allows you to define areas of text that expand/collapse when clicked. It is an improved version of Dagon Design's Expanding Text-Plugin.
 
 == Description ==
 
-Website-elements which are collapsable help avoiding visual noise these days and give control to the user, what they want to see.†Dagon Design developed a WordPress-Plugin called ì<a href="http://www.dagondesign.com/articles/expanding-text-plugin-for-wordpress">Expanding Text</a>î which enables a blogger to make any part of a post collapsable. I wasnít entirely convinced with it, so I forked my own plugin.
+Website-elements which are collapsable help avoiding visual noise these days and give control to the user, what they want to see.¬†Dagon Design developed a WordPress-Plugin called ‚Äú<a href="http://www.dagondesign.com/articles/expanding-text-plugin-for-wordpress">Expanding Text</a>‚Äù which enables a blogger to make any part of a post collapsable. I wasn‚Äôt entirely convinced with it, so I forked my own plugin.
 
 <h4>
     Improvements compared to Expanding Text
@@ -17,10 +17,13 @@ Website-elements which are collapsable help avoiding visual noise these days and
   
   <ul style="margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 30px;">
     <li style="margin: 0px;">
-      A double-arrow is shown next to the expand-link, which indicates whether the part is collapsed or expanded next
+      You can freely choose a text to be shown in front of the expand-link (default is a double-arrow), which indicates whether the part is collapsed or expanded next
     </li>
     <li style="margin: 0px;">
       the expand-part will expand via a jQuery-animation. This looks a lot better, than having the text just appear all of the sudden.
+    </li>
+    <li style="margin: 0px;">
+      You can add expanding text directly with a media-button in the article editor
     </li>
   </ul></span>
 </h4></span>
@@ -29,6 +32,9 @@ Website-elements which are collapsable help avoiding visual noise these days and
 
 Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
 == Changelog ==
-**v0.1**
+= 0.2 =
+* added a media button in the post-editor to add expanding text more directly
+* add settings to customize the text prepending the expand-link
 
-initial release
+= 0.1 =
+* initial release
--- text-expander.php
+++ text-expander.php
@@ -2,11 +2,80 @@
 /*
 Plugin Name: Text Expander
 Plugin URI: http://www.ali.dj/wordpress-plugin-text-expander/
-Description: Allows you to define areas of text that expand/collapse when clicked. It is built on Dagon Design's Expanding Text-Plugin, but comes with some improvements.
+Description: Allows you to define areas of text that expand/collapse when clicked. It is an improved version of Dagon Design's Expanding Text-Plugin.
 Author: Alexander Zigelski
-Version: 0.1
+Version: 0.2
 Author URI: http://www.ali.dj
 */
+
+// add the options page
+add_action('admin_menu', 'text_expander_admin_add_page');
+add_action('wp_head', 'expander_javascript');
+add_filter('the_content', 'expander_process');
+$wp_text_expander_url = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
+
+function text_expander_admin_add_page() {
+add_options_page('Text Expander Settings', 'Text Expander', 'manage_options', 'text_expander', 'text_expander_options_page');
+}
+
+// display the options page
+function text_expander_options_page() {
+?>
+<div>
+<h2>Text Expander Settings</h2>
+Options relating to the Custom Plugin.
+<form action="options.php" method="post">
+<?php settings_fields('text_expander_options'); ?>
+<?php do_settings_sections('text_expander'); ?>
+<br />
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form></div>
+
+<?php
+}
+add_action('admin_init', 'plugin_admin_init');
+
+function plugin_admin_init(){
+register_setting( 'text_expander_options', 'text_expander_options', 'expand_text_validate' );
+add_settings_section('text_expander_main', 'Expand/Collapse-link', 'text_expander_main_section_text', 'text_expander');
+add_settings_field('expand_text', 'Before collapse link:', 'text_expander_expand_text_string', 'text_expander', 'text_expander_main');
+add_settings_field('collapse_text', 'Before expand link:', 'text_expander_collapse_text_string', 'text_expander', 'text_expander_main');
+}
+
+register_activation_hook(__FILE__, 'add_defaults_fn');
+
+// Define default option settings
+function add_defaults_fn() {
+	$tmp = get_option('text_expander_options');
+    if(($tmp['chkbox1']=='on')||(!is_array($tmp))) {
+		$arr = array("expand_text"=>"&uArr;", "collapse_text" => "&dArr;");
+		update_option('text_expander_options', $arr);
+	}
+}
+
+function text_expander_main_section_text() {
+echo '<p>Here you can adjust which character or string is shown before the expand or collapse link.<br />
+		Default values are a down-and up-arrow</p>';
+} 
+
+function text_expander_expand_text_string() {
+$options = get_option('text_expander_options');
+echo "<input id='expand_text_string' name='text_expander_options[expand_text]' size='40' type='text' value='{$options['expand_text']}' />";
+} 
+
+function text_expander_collapse_text_string() {
+$options = get_option('text_expander_options');
+echo "<input id='collapse_text_string' name='text_expander_options[collapse_text]' size='40' type='text' value='{$options['collapse_text']}' />";
+} 
+
+// validate our options
+function expand_text_validate($input) {
+$options = get_option('text_expander_options');
+$options['expand_text'] = trim($input['expand_text']);
+$options['collapse_text'] = trim($input['collapse_text']);
+return $options;
+}
+
 
 function expander_str_replace_once($needle , $replace , $haystack){
     // Looks for the first occurence of $needle in $haystack
@@ -19,6 +88,14 @@ function expander_str_replace_once($needle , $replace , $haystack){
     return substr_replace($haystack, $replace, $pos, strlen($needle));
 }
 
+function wp_text_expander_add_media_button() {
+	GLOBAL $wp_text_expander_url;
+	$url = $wp_text_expander_url.'/media-button-expander.php?tab=add&TB_iframe=true&amp;height=300&amp;width=640';
+	if (is_ssl()) $url = preg_replace( '/^http:\/\//', 'https://',  $url );
+	echo '<a href="'.$url.'" class="thickbox" title="'.'Add Text Expander'.'"><img src="'.$wp_text_expander_url.'/img/media-button-text-expander.gif" alt="'.'Add Text Expander'.'"></a>';
+}
+
+add_action('media_buttons', 'wp_text_expander_add_media_button', 20);
 
 function expander_process($content) {
 
@@ -51,32 +128,29 @@ function expander_process($content) {
 		$new_string .= $sdesc . '</a>' . "\n";
 		$new_string .= '<div class="te_div" id="te' . $rnum . '">';
 		$new_string .= '<script language="JavaScript" type="text/javascript">expander_hide(\'#te' . $rnum . '\');</script>';
-
+		
 		$sdata = preg_replace('`^<br />`sim', '', $sdata);
-
 		$content = expander_str_replace_once($sstring, $new_string . $sdata . '</div>', $content);
-
 		$offset = $s + 1;
 	}
-
 	return $content;
-
 }
 
 function expander_javascript() {
+	$options = get_option('text_expander_options');	
+	
     wp_enqueue_script('jquery');
 	echo '
 
 <script language="JavaScript" type="text/javascript"><!--
 function expand(param) {
 jQuery(function ($) {
- $("div"+param).slideToggle("slow", function() {
-    var linkcontent = $("a"+param).html().substring(1);
+ $("div"+param).stop().slideToggle("slow", function() {
     if( $("div"+param).is(":visible") ) {
-      $("a"+param).html("&uArr;"+linkcontent);
+      $("a"+param).html("'.$options['expand_text'].' "+linkname);
     }
     else {
-        $("a"+param).html("&dArr;"+linkcontent);
+        $("a"+param).html("'.$options['collapse_text'].' "+linkname);	
     }
   });
 });
@@ -84,17 +158,13 @@ jQuery(function ($) {
 function expander_hide(param) {
     jQuery(function ($) {
         $("div"+param).hide();
-        var linkcontent = $("a"+param).html();
-        $("a"+param).html("&dArr; " + linkcontent);
+		linkname = $("a"+param).html();
+        $("a"+param).html("'.$options['collapse_text'].' " + linkname);
         $("a"+param).show();
     });
 }
 //--></script>
 ';
 }
-
-
-add_action('wp_head', 'expander_javascript');
-add_filter('the_content', 'expander_process');
 
 ?>
